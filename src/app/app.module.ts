8329d649834b4f69275fe74594b88f2ef9bf4ddb
Wellington Ideão <wellington.ideao@gmail.com>
2022-08-31 18:35:11 -0300

    Forms reativo: Sincronizando HTML com FormGroup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
-import { DataFormModule } from './data-form/data-form.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { TemplateFormModule } from './template-form/template-form.module';
+import { DataFormModule } from './data-form/data-form.module';
 
 @NgModule({
   declarations: [
